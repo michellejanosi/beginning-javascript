449d35547ad5a397e2a1a860bdeca2be48f3d4b8
michellejanosi <michelle.janosi@gmail.com>
2016-10-09 08:14:39 -0400

    Corrected spelling of celsius

--- 02_1_convert_degrees.js
+++ 02_1_convert_degrees.js
@@ -1,8 +1,8 @@
-/* A JavaScript program to convert degrees Celcius to degrees Fahrenheit and write
+/* A JavaScript program to convert degrees Celsius to degrees Fahrenheit and write
 the result to the page in a descriptive sentence.
-Equation for Fahrenheit to Celcius: degF = 9 / 5 * (degC + 32) */
+Equation for Fahrenheit to Celsius: degF = 9 / 5 * (degC + 32) */
 
-var degF = prompt("What is the temperature in degrees Celcius?", "");
+var degF = prompt("What is the temperature in degrees Celsius?", "");
 var degC = Math.round(9 / 5 * (degC + 32));
 
-alert(degC + "\xB0 Celcius is " + degF + "\xB0 Fahrenheit.");
+alert(degC + "\xB0 Celsius is " + degF + "\xB0 Fahrenheit.");
