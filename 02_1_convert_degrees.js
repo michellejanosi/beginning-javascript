a54da5807d081233cfb269236656fbdbf6580442
michellejanosi <michelle.janosi@gmail.com>
2016-10-11 07:29:18 -0400

    Corrected formula for solution

--- 02_1_convert_degrees.js
+++ 02_1_convert_degrees.js
@@ -1,8 +1,8 @@
 /* A JavaScript program to convert degrees Celsius to degrees Fahrenheit and write
 the result to the page in a descriptive sentence.
-Equation for Fahrenheit to Celsius: degF = 9 / 5 * (degC + 32) */
+Equation for Fahrenheit to Celsius: fahrenheit = 9 / 5 * celsius + 32 */
 
-var degF = prompt("What is the temperature in degrees Celsius?", "");
-var degC = Math.round(9 / 5 * (degC + 32));
+var celsius = prompt("What is the degrees in celsius?", "");
+var fahrenheit = 9 / 5 * celsius + 32;
 
-alert(degC + "\xB0 Celsius is " + degF + "\xB0 Fahrenheit.");
+alert(celsius + "\xB0 Celsius is " + fahrenheit + "\xB0 Fahrenheit.");
